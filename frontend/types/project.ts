a6556af56602142aaa498e0345bdabcbf591f0e6
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-04-29 15:03:00 +0300

    feat: create project page ui

---
+++ frontend/types/project.ts
@@ -0,0 +1,13 @@
+export interface Project {
+  id: number;
+  title: string;
+  skills: string[];
+  period: string;
+  type: string;
+  githubUrl: string;
+  demoUrl: string;
+  imageUrl: string;
+  overview: string;
+  keyFeatures: string[];
+  technicalArchitecture: string[];
+} 
