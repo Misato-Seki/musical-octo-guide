5e2c1b127d944a6ad4f25fe25b3c7ad78c4085ed
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-04-30 19:40:55 +0300

    feat: integrate API for projects page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'http',
+      hostname: (process.env.NEXT_PUBLIC_API_URL || '').replace(/^https?:\/\/|:\d+\/?$/g, ''),
+      port: '8055',
+      pathname: '/assets/**',
+    }],
+  },
 };
 
 export default nextConfig;
