a295c974bbbedbd0c2feb1b4d0c4485c59399418
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-05-04 14:00:52 +0300

    feat: responsive design

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -44,10 +44,10 @@ const NavBar = () => {
                 href={item.path}
                 className="group relative"
               >
-                <span className={`font-medium inline-block transition-all duration-300 ${
+                <span className={`inline-block transition-all duration-300 ${
                   pathname === item.path
-                    ? 'text-black translate-x-2'
-                    : 'text-gray-600 group-hover:text-black group-hover:translate-x-2'
+                    ? 'text-black translate-x-2 font-bold'
+                    : 'text-gray-600 group-hover:text-black group-hover:translate-x-2 group-hover:font-bold'
                 }`}>
                   {item.name}
                 </span>
@@ -63,19 +63,19 @@ const NavBar = () => {
       </nav>
 
       {/* Mobile Navigation */}
-      <nav className="md:hidden fixed top-0 left-0 right-0 z-50 bg-[#171923]">
+      <nav className="md:hidden fixed top-0 left-0 right-0 z-50 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
-              <Link href="/" className="text-xl font-bold text-white/90 hover:text-white transition-colors">
-                Portfolio
+              <Link href="/" className="text-xl font-bold text-gray-600 hover:text-black transition-colors">
+                Misato Seki
               </Link>
             </div>
 
             <div className="md:hidden">
               <button
                 onClick={() => setIsOpen(!isOpen)}
-                className="inline-flex items-center justify-center p-2 rounded-md text-white/60 hover:text-white hover:bg-white/10 focus:outline-none transition-colors"
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-black focus:outline-none transition-colors"
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
@@ -112,7 +112,7 @@ const NavBar = () => {
           </div>
         </div>
 
-        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden transition-all duration-300 ease-in-out bg-[#171923]`}>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden transition-all duration-300 ease-in-out bg-white`}>
           <div className="px-2 pt-2 pb-3 space-y-4 sm:px-3">
             {navItems.map((item) => (
               <Link
@@ -121,14 +121,14 @@ const NavBar = () => {
                 className="group relative block px-4 py-2"
                 onClick={() => setIsOpen(false)}
               >
-                <span className={`font-medium inline-block transition-all duration-300 ${
+                <span className={`inline-block transition-all duration-300 ${
                   pathname === item.path
-                    ? 'text-white translate-x-2'
-                    : 'text-white/60 group-hover:text-white/80 group-hover:translate-x-2'
+                    ? 'text-black translate-x-2 font-bold'
+                    : 'text-gray-600 group-hover:text-black group-hover:translate-x-2 group-hover:font-bold'
                 }`}>
                   {item.name}
                 </span>
-                <span className={`absolute left-0 top-1/2 -translate-y-1/2 h-[1px] bg-white transition-all duration-300 ${
+                <span className={`absolute left-0 top-1/2 -translate-y-1/2 h-[1px] bg-black transition-all duration-300 ${
                   pathname === item.path
                     ? 'w-6 opacity-100'
                     : 'w-2 opacity-40 group-hover:w-4 group-hover:opacity-60'
